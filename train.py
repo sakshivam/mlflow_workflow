1144e5bc932a38569f1966e498be570cdfa32205
artinmajdi <msm2024@gmail.com>
2021-01-24 22:25:08 -0700

    Version2

--- train.py
+++ train.py
@@ -44,20 +44,30 @@ dialect_driver = 'postgresql'
 """ below is the style we should use when running mlflow ui
     server = f'{dialect_driver}://{username}:{password}@{ip}/{database_name}' """
 
-server = f'{dialect_driver}://{username}:{password}@{ip}:{port}/{database_name}'  # :{port}
+server = f'{dialect_driver}://{username}:{password}@{ip}:{port}/{database_name}'
 # server   = "file:/Users/artinmac/Documents/Research/Data7/mlflow/mlrun_store"
 
-# artifact = "file:/Users/artinmac/Documents/Research/Data7/mlflow/artifact_store"
-# artifact = 'sftp://mohammadsmajdi@filexfer.hpc.arizona.edu:/home/u29/mohammadsmajdi/projects/mlflow/artifact_store'
-# artifact = "file:/home/u29/mohammadsmajdi/projects/mlflow/artifact_store"
-artifact = 'sftp://artinmajdi:Rtn1371369!@128.196.142.27:/home/artinmajdi/mlflow/artifact_store'
+""" Setting up the artifact server """ 
+artifact_server = 'atmosphere'
+
+Artifacts = {
+    'local':      "file:/Users/artinmac/Documents/Research/Data7/mlflow/artifact_store",
+    'hpc':        'sftp://mohammadsmajdi@filexfer.hpc.arizona.edu:/home/u29/mohammadsmajdi/projects/mlflow/artifact_store',
+    'atmosphere': 'sftp://artinmajdi:Rtn1371369!@128.196.142.27:/home/artinmajdi/mlflow/artifact_store'}
+
+artifact = Artifacts[artifact_server]
 
 mlflow.set_tracking_uri(server)
 # mlflow.set_registry_uri(server)
 
 """ Creating experiment """
-experiment_name = '/experiment_v2_test_7'
-mlflow.create_experiment(name=experiment_name, artifact_location=artifact)
+ExperimentName = {
+    'local':      '/exp_final_artifact_local',
+    'hpc':        '/exp_final_artifact_atmosphere',
+    'atmosphere': '/exp_final_artifact_hpc'}
+
+experiment_name = ExperimentName[artifact_server]
+# mlflow.create_experiment(name=experiment_name, artifact_location=artifact)
 
 """ Setting the experiment """
 mlflow.set_experiment(experiment_name=experiment_name)
