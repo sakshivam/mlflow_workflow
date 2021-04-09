a84b830badddd63b67b379495f18756fd069afbf
artinmajdi <msm2024@gmail.com>
2021-04-08 17:15:42 -0700

    Versiion 2.4: auto ssh-tunneling in background

--- train.py
+++ train.py
@@ -7,110 +7,120 @@ import mlflow
 import numpy as np
 from time import time
 import git
- 
+import subprocess
+
 # %% ---------------------------------------------------------------------------------------------------------------------
 """ Reading the inputs fed through the command line """
 epochs, batch_size = reading_terminal_inputs()
 # epochs, batch_size = 3,32
 
-# %% ---------------------------------------------------------------------------------------------------------------------
-""" ssh tunneling in the background """
-os.system('ssh -N -L 5000:localhost:5432 artinmajdi@data7-db1.cyverse.org & ')
 
-# %% ---------------------------------------------------------------------------------------------------------------------
-""" MLflow settings: 
+def mlflow_settings(): 
+
+    """ MLflow settings: 
         The style we should use when running mlflow ui
             Postgres server: server = f'{dialect_driver}://{username}:{password}@{ip}/{database_name}' 
             Local:           server = "file:/Users/artinmac/Documents/Research/Data7/mlflow/mlrun_store" """
 
-postgres_connection_type = { 'direct':     ('5432', 'data7-db1.cyverse.org'),
-                             'ssh-tunnel': ('5000', 'localhost')
-                            }
+    postgres_connection_type = { 'direct':     ('5432', 'data7-db1.cyverse.org'),
+                                'ssh-tunnel': ('5000', 'localhost')
+                                }
 
-port, host = postgres_connection_type['ssh-tunnel'] # 'direct' , 'ssh-tunnel'
+    port, host = postgres_connection_type['ssh-tunnel']
+
+    username = "artinmajdi"
+    password = '1234'
+    database_name = 'phyto_oracle_db'
+    dialect_driver = 'postgresql'
+    server = f'{dialect_driver}://{username}:{password}@{host}:{port}/{database_name}'
 
 
-username = "artinmajdi" # e.g., nirav, hagan, nick, ...
-password = '1234'
-database_name = 'phyto_oracle_db'
-dialect_driver = 'postgresql'
+    """ Setting up the artifact server """ 
+    artifact_server = 'data7_db1'
 
-server = f'{dialect_driver}://{username}:{password}@{host}:{port}/{database_name}'
+    Artifacts = {
+        'local':      "file:/<path-to-artifact-store>",
+        'hpc':        'sftp://<user>:{password}@filexfer.hpc.arizona.edu:<path-to-artifact-store>',
+        'atmosphere': 'sftp://<user>:{password}@<ip-address>:<path-to-artifact-store>',
+        'cyverse':    'file:/<path-to-artifact-store>',
+        'data7_db1':  'sftp://artinmajdi:temp2_data7_b@data7-db1.cyverse.org:/home/artinmajdi/mlflow_data/artifact_store'}
 
-# %% ---------------------------------------------------------------------------------------------------------------------
-""" Setting up the artifact server """ 
-artifact_server = 'data7_db1'
+    artifact = Artifacts[artifact_server]
 
-Artifacts = {
-    'local':      "file:/Users/artinmac/Documents/Research/Data7/mlflow/artifact_store",
-    'hpc':        'sftp://mohammadsmajdi@filexfer.hpc.arizona.edu:/home/u29/mohammadsmajdi/projects/mlflow/artifact_store',
-    'atmosphere': 'sftp://artinmajdi:{password}@128.196.142.4:/home/artinmajdi/mlflow/artifact_store',
-    'cyverse':    'file:/Volumes/artinmajdi/projects/mlflow/artifact_store',
-    'data7_db1':  'sftp://artinmajdi:temp2_data7_b@data7-db1.cyverse.org:/home/artinmajdi/mlflow_data/artifact_store'}
+    return server, artifact
 
-artifact = Artifacts[artifact_server]
-
+server, artifact = mlflow_settings()
 mlflow.set_tracking_uri(server)
 
+command = 'ssh -N -L 5000:localhost:5432 artinmajdi@data7-db1.cyverse.org &'
+ssh_session = subprocess.Popen('exec ' + command, stdout=subprocess.PIPE, shell=True)
 
-# %% ---------------------------------------------------------------------------------------------------------------------
-""" Creating/Setting the experiment """
-experiment_name = '/experiment_name'
+try:
+    
+    """ mlflow set-up """
+    # Creating/Setting the experiment
+    experiment_name = '/experiment_name2'
 
-""" Line below should be commented if the experiment is already created
-    If kept commented during the first run of a new experiment, the set_experiment 
-    will automatically create the new experiment with local artifact storage """
-mlflow.create_experiment(name=experiment_name, artifact_location=artifact)
-mlflow.set_experiment(experiment_name=experiment_name)
-
-
-""" Loading the optimization parameters aturomatically from keras """
-mlflow.keras.autolog()
-
-""" Starting the MLflow """
-mlflow.start_run()
+    # Line below should be commented if the experiment is already created
+    # If kept commented during the first run of a new experiment, the set_experiment 
+    # will automatically create the new experiment with local artifact storage
+    # mlflow.create_experiment(name=experiment_name, artifact_location=artifact)
+    mlflow.set_experiment(experiment_name=experiment_name)
 
 
-# %% ---------------------------------------------------------------------------------------------------------------------
-""" Model optimization """
-model = architecture()
+    # Loading the optimization parameters aturomatically from keras
+    mlflow.keras.autolog()
 
-(train_images, train_labels), (test_images, test_labels) = loading_data()
-
-""" model training and evaluation """
-# with mlflow.start_run() as f:  # run_name='run_postgres_r2'experiment_id='7'
-
-start_time = time()
-history = model.fit(train_images, train_labels, epochs=epochs, batch_size=batch_size, validation_data=(test_images, test_labels))
-mlflow.log_metric("Time to optimize and save the model artifact", time()-start_time)
-
-# %% ---------------------------------------------------------------------------------------------------------------------
-""" Model evaluation """
-test_loss, test_acc = model.evaluate(test_images, test_labels)
-print('Accuracy:', test_acc)
-print('Loss: ', test_loss)
-
-prediction = model.predict(test_images)
-predicted_classes = np.argmax(prediction, axis=1)
+    # Starting the MLflow 
+    mlflow.start_run()
 
 
-# %% ---------------------------------------------------------------------------------------------------------------------
-""" Saving MLflow parameters & metrics """
-mlflow.log_param("epochs", epochs)
-mlflow.log_param("bsize", batch_size)
-mlflow.log_metric("accuracy", test_acc)
-mlflow.log_metric("test_loss", test_loss)
+    """ Model optimization """
+    model = architecture()
 
-mlflow.set_tag('User',username)
+    (train_images, train_labels), (test_images, test_labels) = loading_data()
 
 
-# mlflow.keras.log_model(model, "my_model_log")
-# mlflow.keras.save_model(model, 'my_model')
-# with open('predictions.txt', 'w') as f:
-#     f.write("predicted_classes")
-# mlflow.log_artifact('predictions.txt')
-# client.create_registered_model(description='first registered model', name=experiment_name)
+    """ model training and evaluation """
+    # with mlflow.start_run() as f:  # run_name='run_postgres_r2'experiment_id='7'
 
-print("Model saved in run %s" % mlflow.active_run().info.run_uuid)
-# %% ---------------------------------------------------------------------------------------------------------------------
-mlflow.end_run()
+    start_time = time()
+    history = model.fit(train_images, train_labels, epochs=epochs, batch_size=batch_size, validation_data=(test_images, test_labels))
+    mlflow.log_metric("Time to optimize and save the model artifact", time()-start_time)
+
+
+    # Model evaluation
+    test_loss, test_acc = model.evaluate(test_images, test_labels)
+    print('Accuracy:', test_acc)
+    print('Loss: ', test_loss)
+
+    prediction = model.predict(test_images)
+    predicted_classes = np.argmax(prediction, axis=1)
+
+    # Saving MLflow parameters & metrics
+    mlflow.log_param("epochs", epochs)
+    mlflow.log_param("bsize", batch_size)
+    mlflow.log_metric("accuracy", test_acc)
+    mlflow.log_metric("test_loss", test_loss)
+
+    # mlflow.set_tag('User',username)
+    # mlflow.keras.log_model(model, "my_model_log")
+    # mlflow.keras.save_model(model, 'my_model')
+    # with open('predictions.txt', 'w') as f:
+    #     f.write("predicted_classes")
+    # mlflow.log_artifact('predictions.txt')
+    # client.create_registered_model(description='first registered model', name=experiment_name)
+
+    print("Model saved in run %s" % mlflow.active_run().info.run_uuid)
+
+    mlflow.end_run()
+
+
+
+
+finally:
+    # ending the ssh session 
+    # If this failed, we can type 'pkill ssh' in the terminal to kill all ssh sessions
+    ssh_session.kill()
+
+    # os.system('kill %1')
